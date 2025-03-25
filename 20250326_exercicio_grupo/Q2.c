8a2d4ca0a569610f66da97ccaa07040472229181
Efraim Costa <efra10_efraim@hotmail.com>
2025-03-24 22:23:24 -0300

    Questão 2 completa.

--- 20250326_exercicio_grupo/Q2.c
+++ 20250326_exercicio_grupo/Q2.c
@@ -1,3 +1,4 @@
+// Cálculo da TRANSPOSTA de uma matriz quadrada.
 #include <stdio.h>
 #include <stdlib.h>
 #define TAM 4
@@ -13,10 +14,27 @@ int main(int argc, char* argv[]){
     int i = 0;
     int j = 0;
 
-    // Impressão da matriz
+    // Cálculo TRANSPOSTA
+    for(i = 0; i < TAM; i++){
+        for(j = 0; j < TAM; j++){
+            matriz_t[i][j] = matriz[j][i];
+        }
+    }
+
+    // Impressão matriz "normal"
+    printf("\nM\n");
     for(i = 0; i < TAM; i++){
         for(j = 0; j < TAM; j++){
             printf("\t%d\t", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Impressão matriz TRANSPOSTA
+    printf("\nM_T\n");
+    for(i = 0; i < TAM; i++){
+        for(j = 0; j < TAM; j++){
+            printf("\t%d\t", matriz_t[i][j]);
         }
         printf("\n");
     }
