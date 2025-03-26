2e1da77c3acc241ab632e30bf9bc45e6f5635759
Efraim Costa <efra10_efraim@hotmail.com>
2025-03-26 04:39:41 -0300

    Questão 1 com nomes das variáveis melhorados.

--- 20250326_exercicio_grupo/Q1.c
+++ 20250326_exercicio_grupo/Q1.c
@@ -3,29 +3,29 @@
 #define TAM 10
 
 int main(int argc, char* argv[]){
-	int vet_origem[TAM] = {3, 9, 1, -30, 4, 103, 33, 11, 23, 19};
-	int vet_destino[TAM] = {0};
+	int vet[TAM] = {3, 9, 1, -30, 4, 103, 33, 11, 23, 19};
+	int vet_cresc[TAM] = {0};
 	int i = 0, j = 0;
-	int aux = 0;
+	int temp = 0;
 		
 	for(i = 0; i < TAM; i++){
-		vet_destino[i] = vet_origem[i];
-		vet_origem[i] = -1;
+		vet_cresc[i] = vet[i];
+		vet[i] = -1;
 	}
 	
 	for(i = 0; i < TAM - 1; i++){
 		for(j = 0; j < TAM - 1; j++){
-			if(vet_destino[j] > vet_destino[j+1]){
-				aux = vet_destino[j+1];
-				vet_destino[j+1] = vet_destino[j];
-				vet_destino[j] = aux;
+			if(vet_cresc[j] > vet_cresc[j+1]){
+				temp = vet_cresc[j+1];
+				vet_cresc[j+1] = vet_cresc[j];
+				vet_cresc[j] = temp;
 			}
 		}
 	}
 	
 	printf("Vetor DESTINO: [");
 	for(i = 0; i < TAM; i++){
-		printf(" %d ", vet_destino[i]);
+		printf(" %d ", vet_cresc[i]);
 	}
 	printf("]\n");
 	
